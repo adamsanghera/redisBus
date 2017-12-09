3bf2cb6d57133f7d3eed45a4414723e89b7228c3
Adam <as8513@nyu.edu>
2017-12-08 21:44:02 -0500

    some small changes

--- globals.go
+++ globals.go
@@ -1,15 +1,18 @@
 package redisBus
 
 import (
+	"os"
+
 	"github.com/go-redis/redis"
 )
 
 // var redisAddress = "localhost"
 
-os.Getenv("REDIS_ADDRESS")
+var redisAddress = os.Getenv("REDIS_ADDRESS")
+
 // var redisPort = "6379"
 
-os.Getenv("REDIS_PORT")
+var redisPort = os.Getenv("REDIS_PORT")
 
 //Client ...
 // Interface for interacting with redis
